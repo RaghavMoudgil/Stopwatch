178f8d748b4c9d57380cf3ed69fca57b0c74402a
Raghav <112871267+RaghavMoudgil@users.noreply.github.com>
2022-12-16 17:45:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,130 @@
-(function(){
-    var hr=0;
-    var min=0;
-    var sec=0;
-    var displaysec=0;
-    var displaymin=0;
-    var displayhr=0;
+(function () {
+    var hr = 0;
+    var min = 0;
+    var sec = 0;
+    var displaysec = 0;
+    var displaymin = 0;
+    var displayhr = 0;
     var reset_time;
-    var rset=false;
+    var rset = false;
     // var lap = null;
     var lapNow = null;
     function start() {
-        rset= 'true';
+        rset = 'true';
         stopwatch();
     }
     function stop() {
         clearTimeout(reset_time);
     }
     function reset() {
-        rset='false';
-        hr=0;
-        min=0;
-        sec=0;
+        rset = 'false';
+        hr = 0;
+        min = 0;
+        sec = 0;
         clearTimeout(reset_time)
-        document.getElementById('time').innerHTML='00:00:00';
+        document.getElementById('time').innerHTML = '00:00:00';
     }
     function stopwatch() {
-        if(rset=='true'){
+        if (rset == 'true') {
             sec++;
-            if(sec/60==1){
-                sec=0;
+            if (sec / 60 == 1) {
+                sec = 0;
                 min++;
             }
-            if(min/60==1){
-                min=0;
-                sec=0;
+            if (min / 60 == 1) {
+                min = 0;
+                sec = 0;
                 hr++;
-             }
-            if (sec<10){
-                displaysec='0'+sec.toString();
             }
-            else{
-                displaysec=sec;
+            if (sec < 10) {
+                displaysec = '0' + sec.toString();
             }
-            if (min<10){
-                displaymin='0'+min.toString();
+            else {
+                displaysec = sec;
             }
-            else{
-                displaymin=min;
+            if (min < 10) {
+                displaymin = '0' + min.toString();
             }
-            if (hr<10){
-                displayhr='0'+hr.toString();
+            else {
+                displaymin = min;
             }
-            else{
-                displayhr=hr;
+            if (hr < 10) {
+                displayhr = '0' + hr.toString();
             }
-             document.getElementById('time').innerHTML=displayhr+':'+displaymin+':'+displaysec;
-                
+            else {
+                displayhr = hr;
+            }
+            document.getElementById('time').innerHTML = displayhr + ':' + displaymin + ':' + displaysec;
+
         }
-        reset_time=setTimeout(stopwatch, 1000);
+        reset_time = setTimeout(stopwatch, 1000);
 
     }
-        var lap_Btn = document.getElementById('lap');
-        var lap_reset = document.getElementById('lap_reset');
-        var lap_Record = document.getElementById('lap_record');
-        var start_btn=document.getElementById('start');
-        var stop_btn=document.getElementById('stop');
-        var reset_btn= document.getElementById('reset');
-        if (start_btn && stop_btn && reset_btn) {
-            start_btn.addEventListener('click' , start);
-        stop_btn.addEventListener('click' , stop);
-        reset_btn.addEventListener('click' , reset);
-        lap_Btn.addEventListener('click', lap);
-        lap_Btn.addEventListener('click', show_btn);
-        lap_reset.addEventListener('click',reset );
-        }
-        function lap() {
-            lapNow = `<div class="lap"><li>${displayhr} : ${displaymin} : ${displaysec}</li></div>`;
-            lap_Record.innerHTML += lapNow;
-          }
-          function reset(){
-              location.reload();
-          }
-          function show_btn(){
-            document.getElementById('lap_reset').style.display='inline';
-          }
+    var lap_Btn = document.getElementById('lap').style.display = "none";
+    var lap_reset = document.getElementById('lap_reset');
+    var lap_Record = document.getElementById('lap_record');
+    var start_btn = document.getElementById('start');
+    var stop_btn = document.getElementById('stop');
+    var reset_btn = document.getElementById('reset');
+   var count = 0;
+   function counter() {
+    count++
+
+    return count
+   }
+        start_btn.addEventListener('click',() => {    
+            start();
+            counter();
+            if (count >= 1 ){
+                start_btn.disabled = true;
+              
+            }   
+             
+       })
+  
+        stop_btn.addEventListener('click', ()=> {
+            stop();
+            if (start_btn.disabled == true) {
+                start_btn.disabled =false;
+            }
+        });
+        reset_btn.addEventListener('click', ()=> {
+            reset();
+            if (start_btn.disabled == true) {
+                start_btn.disabled =false;
+            }
+        });
+        lap_Btn.addEventListener('click', ()=> {
+            lap();
+            if (start_btn.disabled == true) {
+                start_btn.disabled =false;
+            }
+        });
+        lap_Btn.addEventListener('click', ()=> {
+            show_btn();
+            if (start_btn.disabled == true) {
+                start_btn.disabled =false;
+            }
+        });
+        lap_reset.addEventListener('click', ()=> {
+            reset();
+            if (start_btn.disabled == true) {
+                start_btn.disabled =false;
+            }
+        });
+    
+    function lap() {
         
+        lapNow = `<div class="lap"><li>${displayhr} : ${displaymin} : ${displaysec}</li></div>`;
+        
+        lap_Record.innerHTML += lapNow;
+    }
+    function reset() {
+        location.reload();
+    }
+    function show_btn() {
+        document.getElementById('lap_reset').style.display = 'inline';
+    }
+
 
 })();
